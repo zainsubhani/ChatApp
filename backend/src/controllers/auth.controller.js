1dc64bd3e13390b74d4045fcb76cfffc836d90e7
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-08-12 15:56:17 +0200

    added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,4 @@
-import {
-  ReasonPhrases,
-  StatusCodes,
-  getReasonPhrase,
-  getStatusCode,
-} from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import bcrypt from "bcrypt";
 
 import { userValidationSchema } from "../../validation/validation.user.js";
@@ -16,7 +11,11 @@ export const signup = async (req, res) => {
     const { fullName, email, password, profilePic } = req.body;
 
     const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(StatusCodes.CONFLICT);
+    if (existingUser)
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json({ message: "Email already exists" });
+
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
@@ -27,35 +26,32 @@ export const signup = async (req, res) => {
       profilePic,
     });
 
-    if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
-      res.status(StatusCodes.ACCEPTED).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        email: newUser.email,
-        password: newUser.password,
-        profilePic: newUser.profilePic,
-      });
-    } else {
-      res.status(StatusCodes.CONFLICT).json({ message: "Invalid Data" });
-    }
+    await newUser.save();
+
+    generateToken(newUser._id, res);
+
+    return res.status(StatusCodes.CREATED).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
   } catch (error) {
     console.error("Error in signup:", error);
 
     if (error.isJoi) {
-      // Joi validation errors
       return res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
         errors: error.details.map((e) => e.message),
       });
     }
 
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       message: "Server error, please try again later",
     });
   }
 };
+
 export const signin = (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
